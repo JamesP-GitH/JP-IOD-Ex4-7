8a76e15fb72d159818a04c42186ceeedbf5a4463
JamesP-GitH <james.pagliaccio@gmail.com>
2025-04-30 14:22:37 +1000

    created array of books

--- Ex6_Extension.js
+++ Ex6_Extension.js
@@ -1,3 +1,32 @@
 let books = [
-    {}
-]
+  {
+    title: "Twenty Thousand Leagues Under the Sea",
+    description: "Submarine Adventure.",
+    author: "Jules Verne",
+    pages: 287,
+  },
+  {
+    title: "Moby Dick",
+    description: "A whaling voyage and revenge.",
+    author: "Herman Melville",
+    pages: 635,
+  },
+  {
+    title: "1984",
+    description: "Dystopian future society.",
+    author: "George Orwell",
+    pages: 328,
+  },
+  {
+    title: "The Great Gatsby",
+    description: "The American dream examined.",
+    author: "F. Scott Fitzgerald",
+    pages: 180,
+  },
+  {
+    title: "Pride and Prejudice",
+    description: "A romantic tale of manners.",
+    author: "Jane Austen",
+    pages: 279,
+  },
+];
