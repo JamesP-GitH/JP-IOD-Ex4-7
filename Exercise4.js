77c0be44b12717d103fc8bbde9ba663bd016382a
JamesP-GitH <james.pagliaccio@gmail.com>
2025-04-30 13:42:13 +1000

    added user inputs

--- Exercise4.js
+++ Exercise4.js
@@ -1,5 +1,15 @@
-let x = 10
-let y = 5
+const args = process.argv.slice(2);
+
+if (args.length < 2) {
+    console.log("Please provide two numbers as arguments. Example: node script.js 10 5");
+    process.exit(1);
+}
+
+const x = parseFloat(args[0]);
+const y = parseFloat(args[1]);
+
+// let x = 10
+// let y = 5
 
 function add(a, b) {
     return a + b;
