307bda068ecbdceafdbdc846b5642589c62318ba
JamesP-GitH <james.pagliaccio@gmail.com>
2025-04-30 13:52:49 +1000

    added comments for ex reqs

--- Exercise5.js
+++ Exercise5.js
@@ -1,10 +1,15 @@
+// 1. "create an array that has 5 elements.""
 let colours = ["red", "blue", "green", "yellow", "purple"];
 
+// 2. "Replace the value of the element at position 1 and 4."
 colours[1] = "orange";
 colours[4] = "brown";
 
+// 3. "Add a new element to the beginning of the array"
 colours.unshift("pink");
 
+// 4. "Remove the element at the end of the array"
 colours.pop();
 
+// 5. "Print the array to the console."
 console.log(colours)
