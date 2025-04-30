e27485906c99f6252075b643f8f72f9a6c80f217
JamesP-GitH <james.pagliaccio@gmail.com>
2025-04-30 14:12:06 +1000

    added console prints

--- Exercise6.js
+++ Exercise6.js
@@ -1,6 +1,13 @@
-let Book = {
+let book = {
     "title": "Twenty Thousand Leagues Under the Sea",
     "description": "Submarine Adventure",
     "author": "Jules Verne",
     "pages": "287"
 }
+
+console.log("Title:", book.title);
+console.log("Description:", book.description);
+console.log("Author:", book.author);
+console.log("Pages:", book.pages);
+
+console.log(book)
